615b02f3c54e77aacdfbc1a1d734617a3720ce32
bitbyte08 <me@bitworkspace.kr>
2025-07-14 09:27:58 +0900

    feat :: world 작성

--- launch/slam_gazebo.launch.py
+++ launch/slam_gazebo.launch.py
@@ -1,8 +1,9 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch.substitutions import Command
+from launch.actions import TimerAction
+from launch.substitutions import Command, LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -13,15 +14,28 @@ def generate_launch_description():
     robot_description_content = Command(['xacro ', os.path.join(pkg_dir, 'urdf', 'robot.xacro')])
     robot_description = {'robot_description': robot_description_content}
 
-    urdf_path = os.path.join(pkg_dir, 'urdf', 'robot.xacro')  # 여긴 ros_gz_sim용
-
     return LaunchDescription([
+        # World 파일 인자 선언 (평면 world로 지정)
+        DeclareLaunchArgument(
+            name='world',
+            default_value=os.path.join(pkg_dir, 'worlds', 'world.sdf'),  # 평면 world 파일명
+            description='Gazebo world file'
+        ),
 
-        # Gazebo 실행
+        # Gazebo 설정 (world 포함)
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
-            )
+            ),
+            launch_arguments={'gz_args': ["-r","-s","-v1",LaunchConfiguration('world')]}.items()
+        ),
+
+        # Gazebo GUI 실행
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+            ),
+            launch_arguments={'gz_args': ["-g"]}.items()
         ),
 
         # 로봇 상태 퍼블리셔
@@ -31,13 +45,22 @@ def generate_launch_description():
             output='screen',
             parameters=[robot_description, {'use_sim_time': True}]
         ),
-
-        # Gazebo에 로봇 스폰 (여긴 xacro 파일 경로 그대로 넘김 → ros_gz_sim이 내부에서 파싱 가능할 경우)
-        Node(
-            package='ros_gz_sim',
-            executable='create',
-            arguments=['-name', 'my_robot', '-file', urdf_path],
-            output='screen'
+        
+        # Gazebo에 로봇 스폰 (URDF 문자열 직접 전달)
+        TimerAction(
+            period=5.0,
+            actions=[
+                Node(
+                    package='ros_gz_sim',
+                    executable='create',
+                    arguments=[
+                        '-name', 'my_robot',
+                        '-string', robot_description_content,
+                        '-z', '0.17'
+                    ],
+                    output='screen'
+                )
+            ]
         ),
 
         # 컨트롤러 매니저 실행
